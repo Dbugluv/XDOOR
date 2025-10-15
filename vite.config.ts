6d38a98e07cd972b8500335faad3830208462064
Dbugluv <duanxinyi399@163.com>
2025-10-15 21:40:28 +0800

    feat: last vite version

--- vite.config.ts
+++ vite.config.ts
@@ -11,21 +11,12 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        require('autoprefixer')({
-          overrideBrowserslist: [
-            '> 1%',
-            'last 2 versions',
-            'not dead'
-          ]
-        }),
-        require('postcss-pxtorem')({
-          rootValue: 16, // 基准值，1rem = 16px
-          propList: ['*'], // 需要转换的属性，* 表示所有
-          selectorBlackList: ['.norem'], // 忽略的类名，不进行转换
-          replace: true,
-          mediaQuery: false,
-          minPixelValue: 2, // 最小转换值，小于 2px 不转换
-          exclude: /node_modules/i // 排除 node_modules
+        postcssPxtorem({
+          rootValue: 16,
+          propList: ['*'],
+          // 排除PC端组件或添加媒体查询限制
+          exclude: /(pc-|desktop)/i,
+          mediaQuery: true, // 允许在媒体查询中转换
         })
       ]
     }
